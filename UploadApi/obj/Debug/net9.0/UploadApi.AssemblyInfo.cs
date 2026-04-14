f9f5697752cdc0de013456279447469f45be79e4
Lena2323 <lena.jovicic23@gmail.com>
2026-04-14 08:17:22 +0200

    f

--- UploadApi/obj/Debug/net9.0/UploadApi.AssemblyInfo.cs
+++ UploadApi/obj/Debug/net9.0/UploadApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UploadApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd3e99a0455b002d2d0ed1877fdcae7e58f7b94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e285b61c1e19d0dccc061df4a90ae59d782c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("UploadApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UploadApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
